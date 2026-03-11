443fa77acb42a5400465659b7b2aa3f50482230b
Bettina D'ávila <65546840+medinous@users.noreply.github.com>
2026-03-11 22:17:26 +0000

    Create main App component for Weather Dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Weather Dashboard</h1>
+            {/* Components will go here */}
+        </div>
+    );
+}
+
+export default App;
